49e2239c49cb86152852e42e810dc59487dfd5d3
tony-o <tonyo@skod.it>
2013-01-01 23:14:39 +0000

    init

---
+++ test.js
@@ -0,0 +1,20 @@
+var harmony = require("./lib/harmony");
+var http = require("http");
+
+var s = new harmony.server({persistent:false});
+
+var c1 = new harmony.client({balance:500,listenport:1080});
+var c2 = new harmony.client({balance:500,listenport:1090});
+
+var s1 = http.createServer(function(q,s){
+  console.log(1080);
+  s.end("served from port 1080");
+});
+s1.listen(1080);
+
+var s2 = http.createServer(function(q,s){
+  console.log(1090);
+  s.end("served from port 1090");
+});
+s2.listen(1090);
+
