d89e4a807d9af1ee8db7381c57b6489aaab6a931
Tony O'Dell <tony.odell@live.com>
2013-01-02 14:52:50 -0800

    fixed some buggggs

--- test.js
+++ test.js
@@ -7,13 +7,11 @@ var c1 = new harmony.client({balance:1000,listenport:1080});
 var c2 = new harmony.client({balance:500,listenport:1090});
 
 var s1 = http.createServer(function(q,s){
-  console.log(1080);
   s.end("served from port 1080");
 });
 s1.listen(1080);
 
 var s2 = http.createServer(function(q,s){
-  console.log(1090);
   s.end("served from port 1090");
 });
 s2.listen(1090);
