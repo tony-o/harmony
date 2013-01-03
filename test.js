0a4e4110e2d56083f860848aca2c54c49d4c0cdb
tony-o <tonyo@skod.it>
2013-01-03 02:30:30 +0000

    fixed some bugs, added some cool stuff

--- test.js
+++ test.js
@@ -1,13 +1,16 @@
 var harmony = require("./lib/harmony");
 var http = require("http");
 
-var s = new harmony.server({persistent:false});
+var s = new harmony.server({persistent:false,unavailable:function(e,c){
+  c.write("Unavailable, holmes.");
+}});
 
-var c1 = new harmony.client({balance:1000,listenport:1080});
-var c2 = new harmony.client({balance:500,listenport:1090});
+var c1 = new harmony.client({balance:1000,listenport:1080,ack:60*1000});
+var c2 = new harmony.client({balance:500,listenport:1090,ack:60*1000});
 
 var s1 = http.createServer(function(q,s){
   s.end("served from port 1080");
+  c1.close();
 });
 s1.listen(1080);
 
